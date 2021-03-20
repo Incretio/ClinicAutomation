5b685c57b3ca6ee2573e3320791bb0441f969737
oleg rodin <oleg.rodin@gridnine.com>
2021-03-20 11:11:41 +0300

    init project

---
+++ src/main/java/gb/internship/controller/Demo.java
@@ -0,0 +1,13 @@
+package gb.internship.controller;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+
+@Path("demo")
+public class Demo {
+
+    @GET
+    public String doGet() {
+        return "Application started successfully";
+    }
+}
